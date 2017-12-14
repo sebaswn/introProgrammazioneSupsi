325f72806e670532addcdaef9fc7f5e9e984911e
sebaswnd <sebastiannee@yahoo.com>
2017-12-14 12:29:52 -0800

    aggiunto esercizio 1 di serie 11 e read me con un addNodo bidirezionale

--- Serie11/Bidirezionale/Bidirezionale.java
+++ Serie11/Bidirezionale/Bidirezionale.java
@@ -9,6 +9,18 @@ class List{
   Node head = null;
   Node tail = null;
 
+  List(){
+    addOrdered(11,true);
+    addOrdered(24,true);
+    addOrdered(321,true);
+    addOrdered(43,true);
+    addOrdered(51,true);
+    addOrdered(9,true);
+    addOrdered(21,true);
+    addOrdered(104,true);
+    addOrdered(3,true);
+    addOrdered(56,true);
+  }
   void printList(){
     System.out.println(" ");
 
@@ -24,52 +36,196 @@ class List{
     System.out.println(" ");
   }
 
-  void add(int num, boolean ahead){
-    Node newNode = new Node();
-    if(ahead == true){
-      if (head==null){
-				newNode.next = tail;
-				newNode.prev = null;
-				newNode.number = num;
-        head = newNode;
-			}else{
-        newNode.next = head;
-        newNode.prev = null;
-        newNode.number = num;
-        head = newNode;
+  void remove(double num){
+    if(head == null){
+      System.out.println("Your list is empty.");
+    }else{
+      Node temp = head;
+      Node tempMem = null;
+      Node tempPrev = null;
+
+      while(temp != null && temp.number != num){
+        tempPrev = tempMem;
+        tempMem = temp;
+        temp = temp.next;
       }
-    }else if(ahead == false){
-      if(head == null){
-        add(num, true);
-        return;
+
+      if(temp == null){
+          System.out.println("Your list does not contain this number. Cannot remove it.");
+          return;
+      }
+
+      if(temp == head){
+        head = temp.next;
       }else{
-        newNode.next = null;
-        newNode.prev = tail;
-        newNode.number = num;
-        Node temp = head;
-        while(temp.next != null){
-          temp = temp.next;
+        tempMem.prev = tempPrev;
+        tempMem.next = temp.next;
+        temp.next = null;
+      }
+    }
+  }
+
+  boolean findDuplicate(double num){
+    if(head == null){
+      return false;
+    }else{
+      Node temp = head;
+
+      while(temp != null && temp.number != num){
+        temp = temp.next;
+      }
+
+      if(temp == null){
+          return false;
+      }
+
+      if(temp == head){
+        System.out.println("This number already exists.");
+        return true;
+      }else{
+        System.out.println("This number already exists.");
+        return true;
+      }
+    }
+  }
+
+  void addOrdered(int num, boolean ahead){
+    Node newNode = new Node();
+    Node tempNode = new Node();
+    if(findDuplicate(num) == false){
+      if(ahead == true){
+        if (head==null){
+          newNode.next = tail;
+          newNode.prev = null;
+          newNode.number = num;
+          head = newNode;
+        }else if(head.number > num){
+          newNode.next = head;
+          newNode.prev = null;
+          newNode.number = num;
+          head = newNode;
+
+        }else{
+          tempNode = head;
+          while(tempNode.next != null && tempNode.next.number < num){
+            tempNode = tempNode.next;
+          }
+          newNode.number = num;
+          newNode.next = tempNode.next;
+          if(tempNode.next != null){
+            tempNode.next.prev = newNode;
+          }
+          newNode.prev = tempNode;
+          tempNode.next = newNode;
         }
-        temp.next = newNode;
+      }else{
+        if(tail == null){
+          addOrdered(num, true);
+          return;
+        }else{
+          tempNode = tail;
+          while(tempNode.prev != null && tempNode.prev.number > num){
+            tempNode = tempNode.prev;
+          }
+          newNode.number = num;
+          newNode.prev = tempNode.prev;
+          tempNode.prev.next = newNode;
+          tempNode.prev = newNode;
+          newNode.next = tempNode;
+
         }
       }
     }
+  }
 
+  void find(int num){
+    System.out.println("");
+    if(head == null){
+      System.out.println("Your list is empty, nothing to find.");
+      return;
+    }else{
+      Node tempNode = head;
+
+      while(tempNode != null && tempNode.number != num){
+        tempNode = tempNode.next;
+      }
+
+      if(tempNode == null){
+        System.out.println("Your list does not contain this number, cannot find it.");
+        return;
+      }
+
+      if(tempNode.prev == null){
+        System.out.println("----");
+      }else{
+        System.out.println(tempNode.prev.number);
+      }
+      System.out.println(tempNode.number);
+      if(tempNode.next == null){
+        System.out.println("----");
+      }else{
+        System.out.println(tempNode.next.number);
+      }
+
+    }
+    System.out.println("");
+  }
+
+  void filter(int minNum, int maxNum){
+    int counter = 0;
+    System.out.println("");
+    if(head == null){
+      System.out.println("Your list is empty, nothing to filter.");
+      return;
+    }else{
+      Node tempNode = head;
+      while(tempNode != null){
+        if(tempNode.number > minNum && tempNode.number < maxNum){
+          System.out.println(tempNode.number);
+          counter ++;
+        }else if(tempNode.number> maxNum){
+          break;
+        }
+        tempNode = tempNode.next;
+      }
+    }
+
+    if(counter == 0){
+      System.out.println("There are no numbers in your field");
+    }
+    System.out.println("");
+  }
 }
 
 public class Bidirezionale{
   public static void main(String[] args){
     List listNumeri = new List();
 
-    listNumeri.add(1,true);
-    listNumeri.add(2,true);
-    listNumeri.add(3,true);
-    listNumeri.add(432,false);
-    listNumeri.add(5,false);
-    listNumeri.add(22,true);
 
 
     listNumeri.printList();
+
+    //listNumeri.remove(1);
+
+    listNumeri.addOrdered(423,false);
+    listNumeri.addOrdered(1,false);
+    listNumeri.addOrdered(5,false);
+    listNumeri.addOrdered(352,false);
+    listNumeri.addOrdered(78,false);
+
+    listNumeri.printList();
+
+    listNumeri.remove(43);
+    listNumeri.remove(51);
+    listNumeri.remove(9);
+    listNumeri.remove(21);
+    listNumeri.remove(104);
+
+
+
+    listNumeri.find(5);
+    listNumeri.filter(10,200);
+
 
   }
 }
