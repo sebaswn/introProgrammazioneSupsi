e1d67af8d410866dcb0b4ea71d5ff44c661189bf
sebaswnd <sebastiannee@yahoo.com>
2018-02-21 22:01:39 -0800

    added menu

--- Semester2/Serie/Serie1/Board.java
+++ Semester2/Serie/Serie1/Board.java
@@ -9,6 +9,20 @@ public class Board{
   static final String ANSI_BLUE = "\u001B[34m";
 
 
+  public static int rollDice(){
+    return  1+(int)(Math.random()*6);
+  }
+
+  public static void moving(int moves){
+    while (moves > 0){
+      System.out.println("Which way would you like to move? North(s),East(e),South(s),West(w)");
+      System.out.print("---> ");
+    }
+  }
+
+
+
+
   public static void createBoard(){
     int min = 0;
     int max = 9;
@@ -28,6 +42,8 @@ public class Board{
   }
 
   public static void print(){
+    System.out.println();
+    System.out.println();
     for (int i = 0; i < (gridWidth * 4 +1); i++) {
       System.out.print("-");
     }
@@ -38,15 +54,18 @@ public class Board{
       for (int k = 0; k < gridWidth; k++) {
         if(boardPieces[j][k] == "$"){
           System.out.print(" ");
-          System.out.print(ANSI_YELLOW +boardPieces[j][k]+ ANSI_RESET);
+          //System.out.print(ANSI_YELLOW +boardPieces[j][k]+ ANSI_RESET);
+          System.out.print(boardPieces[j][k]);
           System.out.print(" ");
         }else if(boardPieces[j][k] == "X"){
           System.out.print(" ");
-          System.out.print(ANSI_RED+boardPieces[j][k]+ ANSI_RESET);
+          //System.out.print(ANSI_RED+boardPieces[j][k]+ ANSI_RESET);
+          System.out.print(boardPieces[j][k]);
           System.out.print(" ");
         }else if(boardPieces[j][k] == "Y"){
           System.out.print(" ");
-          System.out.print(ANSI_BLUE +boardPieces[j][k]+ ANSI_RESET);
+          //System.out.print(ANSI_BLUE +boardPieces[j][k]+ ANSI_RESET);
+          System.out.print(boardPieces[j][k]);
           System.out.print(" ");
         }else{
           System.out.print("   ");
@@ -60,5 +79,7 @@ public class Board{
       }
       System.out.println();
     }
+    System.out.println();
+    System.out.println();
   }
 }
--- Semester2/Serie/Serie1/Gioco.java
+++ Semester2/Serie/Serie1/Gioco.java
@@ -1,12 +1,53 @@
+import java.util.Scanner;
+
 public class Gioco {
 
-  public static void main(String[] args) {
-    Board gameBoard = new Board();
-    gameBoard.createBoard();
-    gameBoard.print();
-  }
+  static Board gameBoard = new Board();
+  static boolean play = true;
+  static int player = 1;
+  static Scanner input  = new Scanner(System.in);
 
   public static void menu(){
-    // Working on this !!!!
+
+    System.out.println("Player " + player);
+    System.out.println("0. Exit");
+    System.out.println("1. Show GameBoard");
+    System.out.println("2. Move");
+    System.out.println("3. Show game info");
+    System.out.print("---> ");
+    if(input.hasNextInt()){
+      int choice = input.nextInt();
+
+      switch(choice){
+        case 0:
+          System.out.println("Thank you for playing.");
+          play = false;
+          break;
+        case 1:
+          gameBoard.print();
+          break;
+        case 2:
+          System.out.println("Moving...");
+          break;
+        case 3:
+          System.out.println("Game info...");
+          break;
+      }
+    }
+
+    if(player == 1){
+      player = 2;
+    }else{
+      player = 1;
+    }
+  }
+
+  public static void main(String[] args) {
+
+    gameBoard.createBoard();
+
+    while(play == true){
+      menu();
+    }
   }
 }
